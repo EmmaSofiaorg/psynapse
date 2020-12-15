56566e38fc990ae7f833a7c10e1a0a153fc75f91
leifriksheim <riksheim.leif@gmail.com>
2020-12-15 17:39:59 +0100

    Update next config to support netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   cssModules: false,
+  target: "serverless",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
