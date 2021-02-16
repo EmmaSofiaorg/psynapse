010a68cd03e495a31e6cbbe465687e7e2f3e6d40
leifriksheim <riksheim.leif@gmail.com>
2021-02-16 15:17:27 +0000

    full width text

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -41,6 +41,7 @@ export async function getServerSideProps(context) {
                   heading
                   body
                   columns
+                  full
                 }
                 ... on QuoteRecord {
                   _modelApiKey
